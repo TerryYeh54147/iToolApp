199eb95376d8fae97b86ebcbca2b87c25915e641
terry54147 <0930164@gmail.com>
2024-09-17 16:35:13 +0800

    tune router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Routes, Route } from 'react-router-dom'
 import { Suspense } from 'react'
 import routes from '@/router/index'
 
