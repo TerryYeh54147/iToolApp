f4e731535310b4ff3be1d6e5f9ed9aac6fc46893
terry54147 <0930164@gmail.com>
2024-09-21 09:54:39 +0800

    handle NotFoundPage router issue

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,14 +1,20 @@
 import { Button } from '@mui/material'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const NotFoundPage: React.FC = () => {
+  const navigate = useNavigate()
   return (
     <>
       <div className="flex flex-col items-center justify-center h-screen gap-5">
         <div className="text-center font-bold text-2xl">
           Oops! Page not found.
         </div>
-        <Button variant="contained" color="primary" href="/">
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => navigate('/')}
+        >
           Go to Home
         </Button>
       </div>
